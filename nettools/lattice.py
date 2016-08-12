5ddc48bcbb2646ce68c6188c669b9adeca85f153
Ruggero Cortini <ruggero.cortini@crg.eu>
2016-08-12 18:16:44 +0200

    Lattice draw without labels

--- nettools/lattice.py
+++ nettools/lattice.py
@@ -6,3 +6,5 @@ class Lattice :
         return np.asarray (nx.to_numpy_matrix(self.get_graph()))
     def draw (self) :
         nx.draw_spectral (self.get_graph(),with_labels=True)
+    def get_spectrum (self) :
+        return nx.laplacian_spectrum(self.get_graph())
