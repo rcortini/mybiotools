fd178cdcaa931c932d3c9697b1220b16c75c7f64
Ruggero Cortini <ruggero.cortini@crg.eu>
2016-08-30 15:29:39 +0200

    fixed a few name imports in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .parsers import parse_sam, parse_broadpeak, parse_narrowpeak,\
-                    load_hic_Rao, parse_hic
+                    load_hic_Rao, parse_hic, res_string
 from .utils import error_message, log_message, warn_message
-from .vistools import myboxplot
+from .vistools import myboxplot, plot_hic_matrix, line_plot
 from .hoomdsims import *
 from .nettools import *
 from .cellline import CellLine, Region
