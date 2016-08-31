b92822deb2955e3f414eb01505583799642e6d03
Ruggero Cortini <ruggero.cortini@crg.eu>
2016-08-31 11:11:01 +0200

    new: cell_load_hic

--- __init__.py
+++ __init__.py
@@ -5,5 +5,5 @@ from .vistools import myboxplot, plot_hic_matrix, line_plot
 from .hoomdsims import *
 from .nettools import *
 from .cellline import CellLine, Region
-from .beatolabtools import load_beato_metadata,\
-                           cell_load_tracks
+from .beatolabtools import load_beato_metadata, load_hic_metadata, \
+                           cell_load_tracks, cell_load_hic
--- beatolabtools.py
+++ beatolabtools.py
@@ -2,10 +2,15 @@ import numpy as np
 import pandas as pd
 import os
 from .utils import warn_message
+from .parsers import res_string
 
 def load_beato_metadata (
     metadata_file='/home/rcortini/work/data/beato_lab_metadata.xlsx') :
     return pd.read_excel(metadata_file)
+
+def load_hic_metadata (
+    hic_metadata_file='/home/rcortini/work/data/beato_lab_hic_metadata.xlsx') :
+    return pd.read_excel(hic_metadata_file)
 
 def cell_load_tracks (cell,tracks,xavi_datadir='/mnt/xavi/data') :
     for i,track in tracks.iterrows() :
@@ -36,3 +41,24 @@ def cell_load_tracks (cell,tracks,xavi_datadir='/mnt/xavi/data') :
             cell.load_track(metadata)
         else :
             warn_message('cell_load_tracks','Data not found for %s'%sample_id)
+
+def cell_load_hic (cell,tracks,resolution,
+                   datatype_string='raw',
+                   xavi_hic_datadir='/mnt/hic') :
+    res = res_string (resolution)
+    for i,track in tracks.iterrows() :
+        metadata = track.to_dict()
+        sample_id = track['SAMPLE_ID']
+        metadata['type'] = 'hic'
+        d = "%s/samples/%s/downstream"%(xavi_hic_datadir,sample_id)
+        metadata['fname'] = None
+        if os.path.exists(d) :
+            for root,subdirs,files in os.walk(d) :
+                for f in files :
+                    if datatype_string in f and f.endswith ('%s.tsv.gz'%res) :
+                        fin = '%s/%s'%(root,f)
+                        metadata['fname'] = fin
+        if metadata['fname'] is not None :
+            cell.load_track(metadata)
+        else :
+            warn_message('cell_load_hic','Data not found for %s'%sample_id)
