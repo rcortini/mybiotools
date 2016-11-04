cdcd4df9bb6bb4c73fc1d5f3e023bce1dc465970
Ruggero Cortini <ruggero.cortini@crg.eu>
2016-11-04 15:41:17 +0100

    new: p(s) function in hoomdsims.simanalysis

--- hoomdsims/simanalysis.py
+++ hoomdsims/simanalysis.py
@@ -7,3 +7,12 @@ def hic_chipseq_r2 (hic, chipseq) :
     """
     hic_rowsum = np.sum(H,axis=1)/float(np.sum(H))
     return np.corrcoef(hic_rowsum,chipseq)[0,1]**2
+
+def ps (H) :
+    """
+    Calculate the normalized probability of contact between a monomer and all
+    others as a function of the linear distance s.
+    """
+    p = np.array ([np.mean (np.diagonal (H, offset=k))
+                   for k in range (H.shape[0])])
+    return p/np.sum(p)
