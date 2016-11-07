29d537983cf306bb4095f9c05fda307c494abe00
Ruggero Cortini <ruggero.cortini@crg.eu>
2016-11-07 18:42:30 +0100

    bugfix in hic_chipseq_r2

--- hoomdsims/simanalysis.py
+++ hoomdsims/simanalysis.py
@@ -5,7 +5,7 @@ def hic_chipseq_r2 (hic, chipseq) :
     Calculate the Pearson correlation coefficient between the row sum of the
     given Hi-C matrix and the given ChIP-seq profile.
     """
-    hic_rowsum = np.sum(H,axis=1)/float(np.sum(H))
+    hic_rowsum = np.sum(hic,axis=1)/float(np.sum(hic))
     return np.corrcoef(hic_rowsum,chipseq)[0,1]**2
 
 def ps (H) :
