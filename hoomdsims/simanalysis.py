9bfe1e766bd07980891541ca232e2ecf5319a0cb
Ruggero Cortini <ruggero.cortini@crg.eu>
2016-11-29 17:08:55 +0100

    new: hoomdsims.simanalysis.contacts_with

--- hoomdsims/simanalysis.py
+++ hoomdsims/simanalysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+from MDAnalysis.analysis.distances import distance_array
 
 def hic_chipseq_r2 (hic, chipseq) :
     """
@@ -16,3 +17,33 @@ def ps (H) :
     p = np.array ([np.mean (np.diagonal (H, offset=k))
                    for k in range (H.shape[0])])
     return p/np.sum(p)
+
+def contacts_with (sim,polymer_text,tracers_text,bindingsites_text,teq,tsample,threshold) :
+    """
+    Calculate the relative proportion of contacts of the tracers with binding
+    sites compared with non-binding sites. As usual user should supply
+    equilibration time, sampling time, and contact threshold value.
+    """
+    # select polymer, tracers, and binding sites
+    polymer = sim.u.select_atoms (polymer_text)
+    tracers = sim.u.select_atoms (tracers_text)
+    bss = sim.u.select_atoms (bindingsites_text)
+    # select binding site indices
+    bs_n = bss.n_atoms
+    bs_idx = bss.indices
+    # select non-binding site indices
+    polymer_idx = polymer.indices
+    nbs_idx = np.setdiff1d (polymer_idx,bs_idx)
+    nbs_n = nbs_idx.size
+    # evaluate contacts with binding sites and non-binding sites for each
+    # independent simulation snapshot
+    c = []
+    for i,ts in enumerate(sim.u.trajectory[teq::tsample]) :
+        d = distance_array (polymer.positions,tracers.positions,
+                            box=ts.dimensions)
+        contacts = d<threshold
+        cB = np.sum (contacts[bs_idx]).astype('float')
+        cA = np.sum (contacts[nbs_idx]).astype('float')
+        if cA != 0 :
+            c.append ((cB/cA) / (float(bs_n)/nbs_n))
+    return np.mean(np.array(c))
