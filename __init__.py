65c4cdf41c722ea20bb8715da85e6c35c33689ab
Ruggero Cortini <ruggero.cortini@crg.eu>
2017-03-02 17:20:06 +0100

    new: utils.consecutive_true

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .parsers import parse_sam, parse_broadpeak, parse_narrowpeak,\
                     load_hic_Rao, parse_hic, res_string
-from .utils import error_message, log_message, warn_message
+from .utils import error_message, log_message, warn_message, consecutive_true
 from .vistools import myboxplot, plot_hic_matrix, line_plot, ax_only_y,\
                       color_density_scatter, plot_triangular_matrix
 from .hoomdsims import *
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import time, sys
+import numpy as np
 
 def time_string () :
     return time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime ())
@@ -15,3 +16,12 @@ def log_message (program_name, message) :
 def warn_message (program_name, message) :
     full_message = "%s %s: WARNING: %s"%(time_string (), program_name, message)
     print (full_message)
+
+def consecutive_true(data):
+    """
+    A function that computes the number of consecutive 'True' values in the
+    array 'data'. Taken from http://stackoverflow.com/a/24343375/2312821
+    """
+    return np.diff(np.where(np.concatenate(([data[0]],
+                                     data[:-1] != data[1:],
+                                     [True])))[0])[::2]
