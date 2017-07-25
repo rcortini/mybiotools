33e28a63ed4790d3fa7c69d7ae10af452b7f23a3
Ruggero Cortini <ruggero.cortini@crg.eu>
2017-07-25 11:52:16 +0200

    new: Rao_ENCODE_tools

---
+++ Rao_ENCODE_tools.py
@@ -0,0 +1,4 @@
+def datadir (chromosome_name,resolution,threshold,global_datadir) :
+    return "%s/%s/res_%s/threshold_%d"%(global_datadir,
+                                        chromosome_name,
+                                        threshold)
