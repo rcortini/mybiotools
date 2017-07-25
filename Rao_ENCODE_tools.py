818de07ec188897a06f9cd2c282e8a5b5157881f
Ruggero Cortini <ruggero.cortini@crg.eu>
2017-07-25 11:59:10 +0200

    bugfix in Rao_ENCODE_tools.py

--- Rao_ENCODE_tools.py
+++ Rao_ENCODE_tools.py
@@ -1,4 +1,7 @@
+from .parsers import res_string
+
 def datadir (chromosome_name,resolution,threshold,global_datadir) :
     return "%s/%s/res_%s/threshold_%d"%(global_datadir,
                                         chromosome_name,
+                                        res_string(resolution),
                                         threshold)
