9ffdf6c69fb27c74261138780793c7a0d14bccb6
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-02-14 10:14:13 +0100

    modified width of whiskers in myboxplot

--- vistools.py
+++ vistools.py
@@ -9,8 +9,8 @@ import itertools
 def myboxplot (ax,data,colors) :
     bp = ax.boxplot(data, 1, '')
     plt.setp (bp['boxes'], color='black')
-    plt.setp (bp['whiskers'], color='black', linewidth=2)
-    plt.setp (bp['caps'], color='black', linewidth=2)
+    plt.setp (bp['whiskers'], color='black', linewidth=1)
+    plt.setp (bp['caps'], color='black', linewidth=1)
     plt.setp (bp['medians'], color='black')
     for i,box in enumerate(bp['boxes']) :
         boxCoords = zip(box.get_xdata(),box.get_ydata())
